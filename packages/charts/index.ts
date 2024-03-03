d2e35a5215fdfd1f41abb009fd3f2173ef823a2a
John Brunton <john_brunton@live.co.uk>
2024-03-03 10:26:04 +0000

    refactor: extract AgeingWipChart

--- packages/charts/index.ts
+++ packages/charts/index.ts
@@ -1,4 +1,5 @@
 export * from "./src/cycle-times/scatterplot";
 export * from "./src/cycle-times/histogram";
 export * from "./src/cycle-times/percentiles";
+export * from "./src/cycle-times/ageing-wip-chart";
 export * from "./src/throughput";
