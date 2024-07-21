70bb323720ad3df636cd44a6912b24143d209760
John Brunton <john_brunton@live.co.uk>
2024-07-21 17:31:49 -0400

    fix: epic includeWaitTime on policy form

--- apps/client/src/app/components/edit-cycle-time-policy-form.tsx
+++ apps/client/src/app/components/edit-cycle-time-policy-form.tsx
@@ -105,7 +105,7 @@ export const EditCycleTimePolicyForm: FC<EditCycleTimePolicyForm> = ({
     if (policy.epics.type === "computed") {
       policy.epics = {
         type: "computed",
-        includeWaitTime: false,
+        includeWaitTime: policy.epics.includeWaitTime,
         ...filter,
       };
     }
