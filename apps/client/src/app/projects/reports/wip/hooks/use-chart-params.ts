b303c35e779d3d37f6d5bc4393c4b99b707fc168
John Brunton <1276413+jbrunton@users.noreply.github.com>
2024-11-30 17:03:49 +0000

    feat: improve wip count options

--- apps/client/src/app/projects/reports/wip/hooks/use-chart-params.ts
+++ apps/client/src/app/projects/reports/wip/hooks/use-chart-params.ts
@@ -1,10 +1,12 @@
 import { boolean } from "@agileplanning-io/flow-lib";
 import { z } from "zod";
 import { useChartParamsState } from "../../hooks/use-chart-params";
+import { WipType } from "@agileplanning-io/flow-metrics";
 
 const chartParamsSchema = z.object({
   includeStoppedIssues: boolean.schema.default(boolean.False),
   showPercentileLabels: boolean.schema.default(boolean.True),
+  wipType: z.nativeEnum(WipType).default(WipType.LeadTime),
 });
 
 export type ChartParams = z.infer<typeof chartParamsSchema>;
