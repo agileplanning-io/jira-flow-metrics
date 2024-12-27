133f6a3573af1c463505e98a8a0da96d1c0ba988
John Brunton <1276413+jbrunton@users.noreply.github.com>
2024-12-27 13:38:03 +0000

    refactor: menu items

--- apps/client/src/app/components/policies-dropdown.tsx
+++ apps/client/src/app/components/policies-dropdown.tsx
@@ -8,6 +8,7 @@ import {
   CheckOutlined,
   SaveOutlined,
 } from "@ant-design/icons";
+import { blue } from "@ant-design/colors";
 import { useProjectContext } from "@app/projects/context";
 import {
   Project,
@@ -52,75 +53,36 @@ export const PoliciesDropdown: FC<PoliciesDropdownProps> = ({
     return null;
   }
 
-  const currentPolicy = savedPolicies?.find(
+  const savedPolicy = savedPolicies?.find(
     (policy) => policy.id === savedPolicyId,
   );
 
-  const changed =
-    currentPolicy && !isPolicyEqual(currentPolicy.policy, cycleTimePolicy);
-
-  const canSave = !isNullish(currentPolicy) && changed;
-  const saveReason = canSave
-    ? undefined
-    : isNullish(currentPolicy)
-    ? "Save this policy as a named policy first"
-    : "No policy changes to save";
-
-  const canSaveAs = isNullish(currentPolicy) || changed;
-  const saveAsReason = canSaveAs ? undefined : "No policy changes to save";
-
-  const canMakeDefault = !isNullish(currentPolicy) && !currentPolicy.isDefault;
-  const makeDefaultReason = canMakeDefault
-    ? undefined
-    : isNullish(currentPolicy)
-    ? "Save this policy to make it the default"
-    : "Selected policy is already the default";
-
-  const canDelete = !isNullish(currentPolicy);
-  const deleteReason = canDelete
-    ? undefined
-    : "Save this policy as a named policy first";
-
-  const saveItems: MenuProps["items"] = [
-    {
-      label: <Tooltip title={saveReason}>Save</Tooltip>,
-      key: "Save",
-      disabled: !canSave,
-      onClick: () => {
-        if (currentPolicy) {
-          updatePolicy.mutate({ ...currentPolicy, policy: cycleTimePolicy });
-        }
-      },
+  const { menuItems, changed } = buildPolicyItems({
+    savedPolicy,
+    cycleTimePolicy,
+    onSaveClicked() {
+      if (savedPolicy) {
+        updatePolicy.mutate({ ...savedPolicy, policy: cycleTimePolicy });
+      }
     },
-    {
-      label: <Tooltip title={saveAsReason}>Save As...</Tooltip>,
-      key: "SaveAs",
-      disabled: !canSaveAs,
-      onClick: () => setShowSaveDialog(true),
+    onSaveAsClicked() {
+      setShowSaveDialog(true);
     },
-    {
-      label: <Tooltip title={deleteReason}>Delete...</Tooltip>,
-      key: "Delete",
-      disabled: !canDelete,
-      onClick: () => setShowDeleteDialog(true),
+    onDeleteClicked() {
+      setShowDeleteDialog(true);
     },
-    {
-      label: <Tooltip title={makeDefaultReason}>Make default</Tooltip>,
-      key: "MakeDefault",
-      disabled: !canMakeDefault,
-      onClick: () => {
-        if (currentPolicy) {
-          setDefaultPolicy.mutate(currentPolicy?.id);
-        }
-      },
+    onMakeDefaultClicked() {
+      if (savedPolicy) {
+        setDefaultPolicy.mutate(savedPolicy?.id);
+      }
     },
-  ];
+  });
 
   if (savedPolicies.length) {
-    saveItems.push({ type: "divider" });
+    menuItems.push({ type: "divider" });
   }
 
-  saveItems.push(
+  menuItems.push(
     ...savedPolicies.map((policy) => ({
       label: policy.name,
       key: policy.id,
@@ -132,17 +94,17 @@ export const PoliciesDropdown: FC<PoliciesDropdownProps> = ({
     })),
   );
 
-  const selectedKeys = currentPolicy ? [currentPolicy.id] : [];
+  const selectedKeys = savedPolicy ? [savedPolicy.id] : [];
 
   return (
     <>
       <Space.Compact>
-        <Dropdown menu={{ items: saveItems, selectedKeys }} trigger={["click"]}>
+        <Dropdown menu={{ items: menuItems, selectedKeys }} trigger={["click"]}>
           <Button size="small" icon={<CaretDownOutlined />} iconPosition="end">
-            {currentPolicy?.name ?? (
+            {savedPolicy?.name ?? (
               <Typography.Text type="secondary">Custom</Typography.Text>
             )}
-            <SaveOutlined style={{ color: changed ? undefined : "#AAA" }} />
+            <SaveOutlined style={{ color: changed ? blue.primary : "#AAA" }} />
           </Button>
         </Dropdown>
       </Space.Compact>
@@ -161,10 +123,103 @@ export const PoliciesDropdown: FC<PoliciesDropdownProps> = ({
         projectId={project.id}
         onPolicyDeleted={() => setSavedPolicyId(undefined)}
         onClose={() => setShowDeleteDialog(false)}
-        cycleTimePolicy={currentPolicy}
+        cycleTimePolicy={savedPolicy}
       />
     </>
   );
+};
+
+type BuildPolicyItemsProps = {
+  savedPolicy?: SavedPolicy;
+  cycleTimePolicy: CycleTimePolicy;
+  onSaveClicked: () => void;
+  onSaveAsClicked: () => void;
+  onDeleteClicked: () => void;
+  onMakeDefaultClicked: () => void;
+};
+
+const buildPolicyItems = ({
+  savedPolicy,
+  cycleTimePolicy,
+  onSaveClicked,
+  onSaveAsClicked,
+  onDeleteClicked,
+  onMakeDefaultClicked,
+}: BuildPolicyItemsProps) => {
+  const changed =
+    !isNullish(savedPolicy) &&
+    !isPolicyEqual(savedPolicy.policy, cycleTimePolicy);
+
+  const buildTooltip = (
+    canPerformAction: boolean,
+    tooltip: () => string,
+    label: string,
+  ) => (
+    <Tooltip title={canPerformAction ? undefined : tooltip()}>{label}</Tooltip>
+  );
+
+  const canSave = !isNullish(savedPolicy) && changed;
+  const saveTooltip = buildTooltip(
+    canSave,
+    () =>
+      isNullish(savedPolicy)
+        ? "Save this policy as a named policy first"
+        : "No policy changes to save",
+    "Save",
+  );
+
+  const canSaveAs = isNullish(savedPolicy) || changed;
+  const saveAsTooltip = buildTooltip(
+    canSaveAs,
+    () => "No policy changes to save",
+    "Save As...",
+  );
+
+  const canMakeDefault = !isNullish(savedPolicy) && !savedPolicy.isDefault;
+  const makeDefaultTooltip = buildTooltip(
+    canMakeDefault,
+    () =>
+      isNullish(savedPolicy)
+        ? "Save this policy to make it the default"
+        : "Selected policy is already the default",
+    "Make default",
+  );
+
+  const canDelete = !isNullish(savedPolicy);
+  const deleteTooltip = buildTooltip(
+    canDelete,
+    () => "Save this policy as a named policy first",
+    "Delete...",
+  );
+
+  const menuItems: MenuProps["items"] = [
+    {
+      label: saveTooltip,
+      key: "Save",
+      disabled: !canSave,
+      onClick: onSaveClicked,
+    },
+    {
+      label: saveAsTooltip,
+      key: "SaveAs",
+      disabled: !canSaveAs,
+      onClick: onSaveAsClicked,
+    },
+    {
+      label: deleteTooltip,
+      key: "Delete",
+      disabled: !canDelete,
+      onClick: onDeleteClicked,
+    },
+    {
+      label: makeDefaultTooltip,
+      key: "MakeDefault",
+      disabled: !canMakeDefault,
+      onClick: onMakeDefaultClicked,
+    },
+  ];
+
+  return { menuItems, changed };
 };
 
 type SaveModalProps = {
