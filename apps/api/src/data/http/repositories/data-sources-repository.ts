1678425087e8cdb9111c3594eba8fa14daa244e3
John Brunton <1276413+jbrunton@users.noreply.github.com>
2025-01-18 15:00:03 +0000

    refactor: extract findDataSources

--- apps/api/src/data/http/repositories/data-sources-repository.ts
+++ apps/api/src/data/http/repositories/data-sources-repository.ts
@@ -1,70 +1,12 @@
 import { DataSource, DataSourcesRepository } from "@entities/projects";
 import { Injectable } from "@nestjs/common";
 import { createJiraClient } from "../client/jira-client";
-import { flat } from "remeda";
-import { Version3Client } from "jira.js";
-import { getAllPages } from "../../../../../../packages/data/src/page-utils";
+import { findDataSources } from "@agileplanning-io/flow-data";
 
 @Injectable()
 export class HttpJiraDataSourcesRepository extends DataSourcesRepository {
   async getDataSources({ domain, query }): Promise<DataSource[]> {
-    const normalisedQuery = query?.toLowerCase();
-
     const client = createJiraClient(domain);
-
-    const projects = await this.getProjects(client, query);
-    const filters = await this.getFilters(client, query);
-
-    const dataSources = [...projects, ...filters];
-
-    return dataSources.filter(
-      (dataSource) => dataSource.name?.toLowerCase().includes(normalisedQuery),
-    );
-  }
-
-  private async getFilters(
-    client: Version3Client,
-    query: string,
-  ): Promise<DataSource[]> {
-    const filterPages = await getAllPages((startAt) =>
-      client.filters.getFiltersPaginated({
-        filterName: query,
-        expand: "jql",
-        startAt,
-      }),
-    );
-
-    return flat(
-      filterPages.map(
-        (page) =>
-          page.values?.map((filter) => ({
-            name: filter.name,
-            jql: filter.jql ?? "",
-            type: "filter" as const,
-          })) ?? [],
-      ),
-    );
-  }
-
-  private async getProjects(
-    client: Version3Client,
-    query: string,
-  ): Promise<DataSource[]> {
-    const projectPages = await getAllPages((startAt) =>
-      client.projects.searchProjects({
-        query,
-        startAt,
-      }),
-    );
-
-    return flat(
-      projectPages.map((page) =>
-        page.values.map((project) => ({
-          name: `${project.name} (${project.key})`,
-          jql: `project=${project.key}`,
-          type: "project" as const,
-        })),
-      ),
-    );
+    return findDataSources(client, query);
   }
 }
--- packages/data/index.ts
+++ packages/data/index.ts
@@ -1,1 +1,1 @@
-export * from "./src/page-utils";
+export * from "./src/data-sources";
