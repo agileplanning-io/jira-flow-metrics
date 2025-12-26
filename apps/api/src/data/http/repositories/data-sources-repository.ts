9ecd8bb36c6344fde8a5626708e81732aa6bdbc0
John Brunton <1276413+jbrunton@users.noreply.github.com>
2025-12-26 10:37:11 +0000

    feat: search linear projects

--- apps/api/src/data/http/repositories/data-sources-repository.ts
+++ apps/api/src/data/http/repositories/data-sources-repository.ts
@@ -1,12 +1,24 @@
-import { DataSource, DataSourcesRepository } from "@entities/projects";
+import {
+  DataSource,
+  DataSourcesRepository,
+  SearchDataSourcesParams,
+} from "@entities/projects";
 import { Injectable } from "@nestjs/common";
 import { createJiraClient } from "../client/jira-client";
 import { findDataSources } from "@agileplanning-io/flow-data";
+import { createLinearClient } from "../client/linear-client";
 
 @Injectable()
 export class HttpJiraDataSourcesRepository extends DataSourcesRepository {
   async getDataSources({ domain, query }): Promise<DataSource[]> {
-    const client = await createJiraClient(domain);
+    // console.info({ domain });
+    const client = await this.createClient(domain);
     return findDataSources(client, query);
+  }
+
+  private async createClient(domain: SearchDataSourcesParams["domain"]) {
+    return domain.host === "api.linear.app"
+      ? await createLinearClient(domain)
+      : await createJiraClient(domain);
   }
 }
---
+++ packages/data/src/linear/http-linear-client.ts
@@ -0,0 +1,49 @@
+import { LinearClient, Team } from "@linear/sdk";
+import {
+  BulkIssue,
+  JiraClient,
+  PageFilterDetails,
+  PageProject,
+  SearchAndReconcileResults,
+} from "../jira/jira-client";
+
+export class HttpLinearClient implements JiraClient {
+  constructor(private readonly client: LinearClient) {}
+
+  getFields() {
+    return Promise.resolve([]); // return this.client.issueFields.getFields();
+  }
+
+  getStatuses() {
+    return Promise.resolve([]); // return this.client.workflowStatuses.getStatuses();
+  }
+
+  async enhancedSearch(): Promise<SearchAndReconcileResults> {
+    return Promise.resolve({});
+  }
+
+  fetchIssues(): Promise<BulkIssue> {
+    return Promise.resolve({});
+  }
+
+  async findProjects(): Promise<PageProject> {
+    const teams: Team[] = (await this.client.teams()).nodes;
+    return {
+      values: teams.map((team) => ({
+        id: team.id,
+        key: team.key,
+        name: team.name,
+      })),
+      total: teams.length,
+      maxResults: teams.length,
+    } as PageProject;
+  }
+
+  findFilters(): Promise<PageFilterDetails> {
+    return Promise.resolve({
+      values: [],
+      total: 0,
+      maxResults: 0,
+    } as unknown as PageFilterDetails);
+  }
+}
