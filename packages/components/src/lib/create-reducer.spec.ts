a2b0e5c3e0e48f0e7b962638670ac46d10cca7b1
John Brunton <1276413+jbrunton@users.noreply.github.com>
2026-03-07 21:43:50 +0000

    test: createReducer

--- packages/components/src/lib/create-reducer.spec.ts
+++ packages/components/src/lib/create-reducer.spec.ts
@@ -5,16 +5,26 @@ describe("#createReducer", () => {
     count: number;
   };
 
-  const { reducer, actions } = createImmerReducer({
-    reset: (state: State, _: void) => (state.count = 0),
-    inc: (state: State, _: void) => (state.count += 1),
-  });
+  const reducers = {
+    reset: (state: State) => (state.count = 0),
+    inc: (state: State, { by }: { by?: number }) => (state.count += by ?? 1),
+  };
+
+  const { reducer, actions } = createImmerReducer(reducers);
 
   const initialState = () => ({ count: 0 });
 
   it("returns a reducer over the given actions", () => {
-    const newState = reducer(initialState(), actions.inc());
-    expect(newState).toEqual({ count: 1 });
-    expect(reducer(newState, actions.reset())).toEqual({ count: 0 });
+    const state1 = reducer(initialState(), actions.inc({}));
+    expect(state1).toEqual({ count: 1 });
+
+    const state2 = reducer(state1, actions.inc({}));
+    expect(state2).toEqual({ count: 2 });
+
+    const state3 = reducer(state2, actions.inc({ by: 2 }));
+    expect(state3).toEqual({ count: 4 });
+
+    const state4 = reducer(state3, actions.reset({}));
+    expect(state4).toEqual({ count: 0 });
   });
 });
