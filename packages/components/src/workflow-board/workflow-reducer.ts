8ef5eace2b08d9630b0284d63645465b80bf9491
John Brunton <1276413+jbrunton@users.noreply.github.com>
2026-03-07 14:12:13 +0000

    refactor: extract createReducer function

--- packages/components/src/workflow-board/workflow-reducer.ts
+++ packages/components/src/workflow-board/workflow-reducer.ts
@@ -5,35 +5,7 @@ import {
   WorkflowStageColumn,
 } from "./workflow-state";
 
-type AnyFn<S> = (state: S, params: any) => S;
-type Params<H extends AnyFn<any>> = H extends (
-  state: any,
-  params: infer P,
-) => any
-  ? P
-  : never;
-
-function createReducer<S>() {
-  return <H extends Record<string, AnyFn<S>>>(handlers: H) => {
-    type Action = {
-      [K in keyof H & string]: { type: K } & Params<H[K]>;
-    }[keyof H & string];
-    return {
-      reducer: (state: S, action: Action): S =>
-        (handlers[action.type as keyof H] as AnyFn<S>)(state, action as any),
-      actions: Object.fromEntries(
-        Object.keys(handlers).map((key) => [
-          key,
-          (params: object) => ({ type: key, ...params }),
-        ]),
-      ) as unknown as {
-        [K in keyof H & string]: (
-          params: Params<H[K]>,
-        ) => { type: K } & Params<H[K]>;
-      },
-    };
-  };
-}
+import { createReducer } from "../create-reducer";
 
 export const { reducer: workflowStateReducer, actions: workflowActions } =
   createReducer<WorkflowState>()({
